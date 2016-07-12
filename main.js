b95f278b729da527db8dd4f66ea9206767a4b402
lora2016 <15543120073@163.com>
2016-07-12 08:01:35 +0800

    css3 filter
    css3 filter 十种效果

---
+++ main.js
@@ -0,0 +1,26 @@
+function getFileUrl(sourceId) {
+	var url;
+	if (navigator.userAgent.indexOf("MSIE") >= 1) { // IE 
+		url = document.getElementById(sourceId).value;
+	} else if (navigator.userAgent.indexOf("Firefox") > 0) { // Firefox 
+		url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+	} else if (navigator.userAgent.indexOf("Chrome") > 0) { // Chrome 
+		url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+	}
+	return url;
+}
+
+/** 
+ * 将本地图片 显示到浏览器上 
+ */
+function preImg(sourceId, targetId) {
+	var url = getFileUrl(sourceId);
+	var imgPre = document.getElementById(targetId);
+	imgPre.src = url;
+
+	var gallery = document.getElementById("gallery");
+	var imgs = gallery.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++) {
+		imgs[i].src = url;
+	}
+}
